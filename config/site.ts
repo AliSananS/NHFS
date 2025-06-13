bd01de5e5b2476e13aad0dfa593f04f4f4ee2751
Ali Sanan <107338507+AliSananS@users.noreply.github.com>
2025-06-13 09:00:25 +0000

    Initial Commit.

---
+++ config/site.ts
@@ -0,0 +1,10 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "NHFS",
+  description:
+    "NHFS — Next.js HTTP File Server. 📁 Upload. 📤 Download. 🌍 Access from anywhere.",
+  navItems: [],
+  navMenuItems: [],
+  links: {},
+};
